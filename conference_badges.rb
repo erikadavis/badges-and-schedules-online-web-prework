065df1088e745b39664dec11311bdab159361604
Erika Davis <davis2erika@gmail.com>
2019-05-06 22:10:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,5 +18,9 @@ def printer(names)
   badges = batch_badge_creator(names)
   badges.each do |badge|
     puts badge
+    room_assignments = assign_rooms(names)
+    room_assignments.each_with_index do |speaker, index|
+      puts "#{speaker}, #{index}"
+    end
   end
 end
